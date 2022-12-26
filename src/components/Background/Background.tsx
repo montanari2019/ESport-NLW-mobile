45aced7433eb9e69063abd6392789981925c49c5
@ikaromontanarib3 <desenvolvimento4@grupoib3.com.br>
2022-12-26 15:50:31 -0400

    tela inicial feita

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -2,7 +2,7 @@
 import { ImageBackground } from 'react-native';
 
 import { styles } from './styles';
-import bacckgroundImg from "../../assets/background-galaxy.png"
+import backgroundImg from "../../assets/background-galaxy.png"
 
 interface BackgroundProps{
   children: React.ReactNode
@@ -10,7 +10,7 @@ interface BackgroundProps{
 
 export function Background({ children }: BackgroundProps) {
   return (
-    <ImageBackground source={bacckgroundImg} defaultSource={bacckgroundImg} style={styles.container} >
+    <ImageBackground source={backgroundImg} defaultSource={backgroundImg} style={styles.container} >
       {children}
     </ImageBackground>
   );
